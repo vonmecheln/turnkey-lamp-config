c258fa2413c7b72733517185f248cd6e97237032
Luís Henrique Manosso Von Mecheln <luishenrique.si@gmail.com>
2019-09-19 15:08:41 -0300

    apache AllowOverride

--- config.sh
+++ config.sh
@@ -1,3 +1,9 @@
 sed -i -e '/eth1/s/#//' /etc/network/interfaces
 
 sed -i -e '/display_errors/s/Off/On/' /etc/php/7.0/apache2/php.ini
+
+sed -i -e '/^\tAllowOverride/s/Nome/All/' /etc/apache2/apache2.conf
+
+a2enmod rewrite
+
+
