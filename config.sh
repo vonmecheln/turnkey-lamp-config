7cd00aa886ac91769cd4de711ab107c712eab5fc
Luís Henrique Manosso Von Mecheln <luishenrique.si@gmail.com>
2019-09-19 14:42:52 -0300

    display errors On

--- config.sh
+++ config.sh
@@ -1,3 +1,3 @@
 sed -i -e '/eth1/s/#//' /etc/network/interfaces
 
-sed -i -e '/display_errors/s/Off/On' /etc/php/7.0/apache2/php.ini
+sed -i -e '/display_errors/s/Off/On/' /etc/php/7.0/apache2/php.ini
